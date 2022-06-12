805819d1dcaedc72383c0bf002300cc16b6e6d71
hayahaha <duyhm1998@gmail.com>
2022-06-12 14:24:01 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function submit() {
+    setCount(count + 1);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={submit}>submit</button>
+        <h1>{count}</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          hello React
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = screen.getByText(/hello react/i);
+  expect(text).toBeInTheDocument();
+});
+
+test("It should have a default initial value of 0", () => {
+  render(<App />);
+  const count = screen.queryByText(0);
+  expect(count).toBeVisible();
+});
+
+test("It should increase value by one when click submit", () => {
+  render(<App />);
+  fireEvent.click(screen.getByText("submit"));
+  const count = screen.queryByText(1);
+  expect(count).toBeVisible();
 });
